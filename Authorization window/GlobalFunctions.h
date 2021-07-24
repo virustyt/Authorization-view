a01f9c1487fba8202e5a6de8c03ebde8bef55a09
Vladimir Oleinikov <dragon2020@inbox.ru>
2021-07-24 19:53:53 +0300

    feat: create file for global functions

---
+++ Authorization window/GlobalFunctions.h
@@ -0,0 +1,11 @@
+//
+//  GlobalFunctions.h
+//  Authorization window
+//
+//  Created by Vladimir Oleinikov on 24.07.2021.
+//
+
+@import UIKit;
+
+CGRect CGRectMyOssomMake(CGFloat x, CGFloat y, CGFloat width, CGFloat height);
+
---
+++ Authorization window/GlobalFunctions.m
@@ -0,0 +1,15 @@
+//
+//  GlobalFunctions.m
+//  Authorization window
+//
+//  Created by Vladimir Oleinikov on 24.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+#import "GlobalFunctions.h"
+
+CGRect CGRectMyOssomMake(CGFloat x, CGFloat y, CGFloat width, CGFloat height) {
+    NSLog(@"I am Ossom!");
+    return CGRectMake(x, y, width, height);
+}
+
