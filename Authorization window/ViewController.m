ac9aace362d738c5ef52db9922e60494e7a6a6f7
Vladimir Oleinikov <dragon2020@inbox.ru>
2021-07-24 19:54:33 +0300

    feat: create title constants

--- Authorization window/ViewController.m
+++ Authorization window/ViewController.m
@@ -6,6 +6,13 @@
 //
 
 #import "ViewController.h"
+//#import "UIColorAppColors.h"
+
+static const NSString *gPageTitle = @"RSSchool";
+static const NSString *gLoginFieldPlaceholder = @"Login";
+static const NSString *gPasswordFieldPlaceholder = @"Password";
+static const NSString *gAuthorizationButtonTitle = @"Authorize";
+
 
 @interface ViewController ()
 
@@ -13,9 +20,13 @@
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
 }
 
 
