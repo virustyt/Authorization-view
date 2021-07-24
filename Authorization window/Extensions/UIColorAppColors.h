4ba82f4b87ee4c9e5312d4cdfd37f6e07da92240
Vladimir Oleinikov <dragon2020@inbox.ru>
2021-07-24 09:28:51 +0300

    feat: create static colors in UIColor

---
+++ Authorization window/Extensions/UIColorAppColors.h
@@ -0,0 +1,25 @@
+//
+//  UIColorExpression.h
+//  Authorization window
+//
+//  Created by Vladimir Oleinikov on 23.07.2021.
+//
+@import Foundation;
+@import UIKit;
+
+@interface UIColor (AppColors)
+//+(UIColor *) white;
+//+(UIColor *) blackCoral;
+//+(UIColor *) turquoiseGreen;
+//+(UIColor *) venetianRed;
+//+(UIColor *) black;
+//+(UIColor *) littleBoyBlue;
+
+@property (class,nonatomic,readonly) UIColor *white;
+@property (class,nonatomic,readonly) UIColor *blackCoral;
+@property (class,nonatomic,readonly) UIColor *turquoiseGreen;
+@property (class,nonatomic,readonly) UIColor *venetianRed;
+@property (class,nonatomic,readonly) UIColor *black;
+@property (class,nonatomic,readonly) UIColor *littleBoyBlue;
+
+@end
---
+++ Authorization window/Extensions/UIColorAppColors.m
@@ -0,0 +1,36 @@
+//
+//  UIColorExtension.m
+//  Authorization window
+//
+//  Created by Vladimir Oleinikov on 23.07.2021.
+//
+
+#import "UIColorAppColors.h"
+
+@implementation UIColor (AppColors)
+
++(UIColor *) white {
+    return [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];;
+};
+
++(UIColor *) blackCoral {
+    return [UIColor colorWithRed:76.0/255.0 green:92.0/255.0 blue:104.0/255.0 alpha:1.0];
+};
+
++(UIColor *) turquoiseGreen {
+    return [UIColor colorWithRed:145.0/255.0 green:199.0/255.0 blue:177.0/255.0 alpha:1.0];
+};
+
++(UIColor *) venetianRed {
+    return [UIColor colorWithRed:194.0/255.0 green:1.0/255.0 blue:20.0/255.0 alpha:1.0];
+};
+
++(UIColor *) black {
+    return [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+};
+
++(UIColor *) littleBoyBlue {
+    return [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+};
+
+@end
