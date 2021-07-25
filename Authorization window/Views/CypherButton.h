442f963f73f8e41d8f0a0054e08b974ee2be622f
Vladimir Oleinikov <dragon2020@inbox.ru>
2021-07-25 18:23:57 +0300

    feat: create view for cypher button

---
+++ Authorization window/Views/CypherButton.h
@@ -0,0 +1,24 @@
+//
+//  CypherButton.h
+//  Authorization window
+//
+//  Created by Vladimir Oleinikov on 25.07.2021.
+//
+
+@import Foundation;
+@import UIKit;
+
+typedef NS_ENUM(NSInteger, CypherButtonCases) {
+    One = 1,
+    Two,
+    Three
+};
+
+#ifndef CypherButton_h
+#define CypherButton_h
+
+@interface CypherButton : UIButton
+@property (assign,nonatomic,readonly) CypherButtonCases cypherCase;
+@end
+
+#endif /* CypherButton_h */
---
+++ Authorization window/Views/CypherButton.m
@@ -0,0 +1,59 @@
+//
+//  CypherButton.m
+//  Authorization window
+//
+//  Created by Vladimir Oleinikov on 25.07.2021.
+//
+
+#import "CypherButton.h"
+#import "UIColor+AdditionalColors.h"
+
+static const CGFloat backgroundOpacity = 0.2;
+
+@interface CypherButton ()
+@property (assign,nonatomic,readwrite) CypherButtonCases cypherCase;
+@end
+
+@implementation CypherButton
+
+-(CGSize)intrinsicContentSize {
+    return  CGSizeMake(50.0, 50.0);
+}
+
+-(void)setHighlighted:(BOOL)highlighted {
+    if (self.isHighlighted) {
+        self.layer.backgroundColor = [UIColor.littleBoyBlue colorWithAlphaComponent:backgroundOpacity].CGColor;
+    } else {
+        self.layer.backgroundColor = UIColor.clearColor.CGColor;
+    }
+}
+
+-(id)initWithCoder:(NSCoder *)coder {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"init(coder:) has not been implemented" userInfo:nil];
+}
+
+-(id)init:(CypherButtonCases) buttonCase {
+    self = [super initWithFrame:CGRectZero];
+    
+    if(self) {
+        self.cypherCase = buttonCase;
+        
+        NSString *title = [[NSString alloc] initWithFormat:@"%ld",(long)self.cypherCase];
+        
+        [self setTitle: title forState:UIControlStateNormal];
+        self.titleLabel.font = [ UIFont systemFontOfSize:30 weight:UIFontWeightMedium];
+        [self setTitleColor:UIColor.littleBoyBlue forState:UIControlStateNormal];
+        
+        self.layer.cornerRadius = 25.0;
+        self.layer.borderWidth = 1.5;
+        self.layer.borderColor = UIColor.littleBoyBlue.CGColor;
+        
+        [self setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+        [self setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+        
+    }
+    return self;
+}
+                                
+
+@end
